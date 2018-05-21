31a9586152a3e909908fb489a01829c1128727de
Javier <16022934@myrp.edu.sg>
2018-05-21 11:59:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022934/demodatabasecrud/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.a16022934.demodatabasecrud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd, btnEdit, btnRetrieve;
+    EditText etContent;
+    ArrayList<String> al;
+    ListView lv;
+    ArrayAdapter aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnEdit = findViewById(R.id.btnEdit);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
+        etContent = findViewById(R.id.etContent);
+        lv = findViewById(R.id.lv);
+        al = new ArrayList<String>();
+        aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, al);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etContent.getText().toString();
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long row_affected = dbh.insertNote(data);
+                dbh.close();
+
+                if (row_affected != -1){
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                    btnRetrieve.performClick();
+                }
+
+            }
+        });
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!al.isEmpty()) {
+                    Intent i = new Intent(MainActivity.this,
+                            Edit.class);
+
+                    String data = al.get(0);
+                    String id = data.split(",")[0].split(":")[1];
+                    String content = data.split(",")[1].trim();
+
+                    Note target = new Note(Integer.parseInt(id), content);
+                    i.putExtra("data", target);
+                    startActivityForResult(i, 9);
+                }
+            }
+        });
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int
+                    position, long identity) {
+                Intent i = new Intent(MainActivity.this,
+                        Edit.class);
+                String data = al.get(position);
+                String id = data.split(",")[0].split(":")[1];
+                String content = data.split(",")[1].trim();
+
+                Note target = new Note(Integer.parseInt(id), content);
+                i.putExtra("data", target);
+                startActivityForResult(i, 9);
+            }
+        });
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                al.clear();
+                al.addAll(dbh.getAllNotes());
+                dbh.close();
+
+                String txt = "";
+                for (int i = 0; i< al.size(); i++){
+                    String tmp = al.get(i);
+                    txt += tmp + "\n";
+                }
+                aa.notifyDataSetChanged();
+            }
+        });
+
+
+        lv.setAdapter(aa);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 9){
+            btnRetrieve.performClick();
+        }
+    }
+
+}
